c2897646921a0e7ecefbed7055d036d68f444553
Spacevash <nidhi.gaday@gmail.com>
2017-09-18 16:13:11 -0400

    updated data binding and content for infowindow

--- src/js/appEngine.js
+++ src/js/appEngine.js
@@ -8,13 +8,12 @@ var loadScript = function() {
   JSElement.async;
   JSElement.defer;
   JSElement.onerror = function() {
-    $('.sec_map').text("Oops! Google maps could not be loaded, please try again later.");
+    $('.container').text("Oops! Google maps could not be loaded, please check the API Url.");
   };
-  document.getElementsByTagName('body')[0].appendChild(JSElement);
+  $('body')[0].appendChild(JSElement);
 };
 
 loadScript();
-
 
 
 /*==========All Variables===========*/
@@ -22,185 +21,26 @@ loadScript();
 var map, windowInfo, contentStr, vm;
 
 
-/*-----checks if windowInfo is null-----*/
-function checkinfowindow() {
+/*-----initialize infowindow-----*/
+function initInfowindow() {
   //Create infowindow for the marker
   windowInfo = new google.maps.InfoWindow();
+  windowInfo.addListener('closeclick', function() {
+    //close wiki error message
+    if (vm.wikiError()) {
+      vm.wikiError(false);
+    }
+    //close wiki data div
+    vm.wikidata([]);
+    this.close();
+    initInfowindow();
+  });
 }
 
 
-/*======================Model=========================*/
+/*=======Creating map===========*/
 
-var LocModel = function(data, index) {
-
-  var self = this;
-  //information required for list fiter computation
-  this.location = ko.observable(data.location);
-  this.title = ko.observable(data.title);
-  this.zip = ko.observable(data.pin);
-  this.season = ko.observable(data.season);
-  this.episode = ko.observable(data.episode);
-  this.loc = ko.observable(data.loc);
-  this.city = ko.observable(data.city);
-  this.id = ko.observable(index);
-  this.fav = ko.observable();
-
-  //default marker pin with custom size
-  this.defaultIcon = {
-    url: 'src/icons/Map-Marker-pushpin-red.png',
-    size: new google.maps.Size(50, 44),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(10, 44),
-    scaledSize: new google.maps.Size(50, 44)
-  };
-
-  //custom marker pin when hovered on marker pin
-  this.highlightIcon = {
-    url: 'src/icons/Map-Marker-pushpin-green.png',
-    size: new google.maps.Size(50, 44),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(10, 44),
-    scaledSize: new google.maps.Size(50, 44)
-  };
-
-  this.contentStr = '<div class="infoStyle">' +
-    '<div class="infoTitle">' + this.loc() +
-    '</div><div><span class="bold">' + this.episode() + ' - ' +
-    '</span><span>' + this.title() +
-    '</span></div><div>' + this.zip() +
-    '</div><div id="infoPanoImage">No Street View Found' +
-    '</div><img id="locImg" src="">' +
-    '</div>';
-
-  //create marker for each location
-  this.marker = new google.maps.Marker({
-    position: data.location,
-    title: data.title,
-    loc: data.loc,
-    zip: data.pin,
-    icon: this.defaultIcon,
-    episode: data.episode,
-    animation: google.maps.Animation.DROP,
-    id: self.id,
-    city: data.city,
-    visible: false
-  });
-
-  //function defination to setup content in infowindow
-  this.getStreetView = function(val, status) {
-
-    //OK means, image found in given location
-    if (status === google.maps.StreetViewStatus.OK) {
-
-      //data passed here is used to get lat lng, radius
-      var nearLoc = val.location.latLng;
-      //calculate heading for the panorama image option
-      var heading = google.maps.geometry.spherical.computeHeading(
-        nearLoc, self.marker.position);
-      var lat = nearLoc.lat();
-      var lng = nearLoc.lng();
-
-      //set content in InfoWindow
-      windowInfo.setContent(self.contentStr);
-
-      //StreetViewPanoramaOptions
-      var panoOptions = {
-        position: nearLoc,
-        pov: {
-          heading: heading,
-          pitch: 10
-        }
-      };
-
-      //create Panorama image for width higher than 768px
-      var panorama = new google.maps.StreetViewPanorama(document.getElementById(
-        'infoPanoImage'), panoOptions);
-
-      //getting static street image for mobile views
-      var staticImage =
-        'https://maps.googleapis.com/maps/api/streetview?size=300x300&location=' +
-        lat + ',' + lng + '&heading' + heading + '&pitch=20&key=' + myMapsKey;
-      $('#locImg').attr('src', staticImage);
-
-    } else {
-      windowInfo.setContent(self.contentStr);
-      $('#locImg').css('display', 'none');
-      $('#infoPanoImage').css('min-height', 'auto');
-    }
-  }; //end of getStreetView()
-
-
-  //function to assign values to the InfoWindow properties
-  this.populateInfo = function(thisMarker, infoWindow) {
-    if (infoWindow.marker != thisMarker) {
-
-      //using StreetViewService instead of Panorama
-      //in-case image of given latLng is not available
-      var streetView = new google.maps.StreetViewService();
-
-      //setting the radius to find image in meters
-      var radius = 50;
-
-      //default method in prototype of google.maps.StreetViewService
-      //getPanoramaByLocation(latlng, radius, callback fn)
-      streetView.getPanoramaByLocation(thisMarker.position, radius,
-        self.getStreetView);
-
-      //open content in infoWindow at anchor - thisMarker on 'map'
-      infoWindow.open(map, thisMarker);
-    }
-  }; //end of populateInfo()
-
-  //function defination to bounce pin when clicked
-  this.toggleBounce = function(pin) {
-    if (pin.getAnimation() !== null) {
-      pin.setAnimation(null);
-    } else {
-      pin.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function() {
-        pin.setAnimation(null);
-      }, 1450);
-    }
-  };
-
-
-  /*------Set of functions for Mouse events for marker pin------*/
-
-  //adding a function to hover-in event to change marker icon
-  this.marker.addListener('mouseover', function() {
-    this.setIcon(self.highlightIcon);
-  });
-
-  //adding a function to hover-out event to change marker icon
-  this.marker.addListener('mouseout', function() {
-    this.setIcon(self.defaultIcon);
-  });
-
-  //adding a function to click event to display InfoWindow for each marker
-  this.marker.addListener('click', function() {
-    self.clickMarker(this, windowInfo);
-  });
-  /*------Set of functions END------*/
-
-
-  //function defination click callback
-  //keeping this separate so that it can be called when list item is clicked
-  this.clickMarker = function(val, infoWindow) {
-    // val is the clicked marker
-    self.toggleBounce(val);
-    self.populateInfo(val, infoWindow);
-  }; // end of function clickMarker()
-
-}; // end of locModel function DEFINATION
-
-
-
-
-/*============View Model===========*/
-
-var ViewModel = function() {
-  var self = this;
-
+var getMap = function() {
   //Create new map
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
@@ -224,9 +64,197 @@ var ViewModel = function() {
   //set custom map
   map.mapTypes.set('custom_map', styledMapType);
   map.setMapTypeId('custom_map');
+};
 
-  //initializing loactions array
+
+/*=======Wikipedia data function=======*/
+
+
+//function to get Wikipedia data
+var getWiki = function(location) {
+
+  //remove existing wiki data
+  vm.wikidata([]);
+
+  var term = 'Supernatual ' + location.season;
+  var Wikiurl = "https://en.wikpedia.org/w/api.php?action=opensearch&format=json&search=" + term;
+
+  $.ajax({
+    url: Wikiurl,
+    dataType: 'jsonp',
+  }).done(function(data) {
+    vm.wikidata.push({
+      title: ko.observable(data[0]),
+      src: ko.observable(data[3][0]),
+      snippet: ko.observable(data[2][0])
+    });
+  }).fail(function(textStatus) {
+    if (textStatus == 'error') {
+      vm.wikiError(true);
+    }
+  });
+}; //end of getWiki()
+
+
+/*======================Model=========================*/
+
+var LocModel = function(data, index) {
+
+  var self = this;
+  //information required for list fiter computation
+  //and infoWindow
+  this.title = data.title;
+  this.zip = data.pin;
+  this.season = data.season;
+  this.episode = data.episode;
+  this.loc = data.loc;
+
+  //default marker pin with custom size
+  this.defaultIcon = {
+    url: 'src/icons/Map-Marker-pushpin-red.png',
+    size: new google.maps.Size(50, 44),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(10, 44),
+    scaledSize: new google.maps.Size(50, 44)
+  };
+
+  //custom marker pin when hovered on marker pin
+  this.highlightIcon = {
+    url: 'src/icons/Map-Marker-pushpin-green.png',
+    size: new google.maps.Size(50, 44),
+    origin: new google.maps.Point(0, 0),
+    anchor: new google.maps.Point(10, 44),
+    scaledSize: new google.maps.Size(50, 44)
+  };
+
+  this.contentStr = '<div class="infoStyle">' +
+    '<div class="infoTitle">' + this.loc +
+    '</div><div><span class="bold">' + this.episode + ' - ' +
+    '</span><span>' + this.title +
+    '</span></div><div>' + this.zip;
+
+
+  //create marker for each location
+  this.marker = new google.maps.Marker({
+    position: data.location,
+    icon: this.defaultIcon,
+    animation: google.maps.Animation.DROP,
+    season: data.season,
+    visible: false
+  });
+
+  //function defination to setup content in infowindow
+  this.getStreetView = function(val, status) {
+    //OK means, image found in given location
+    if (status === google.maps.StreetViewStatus.OK) {
+      //data passed here is used to get lat lng, radius
+      var nearLoc = val.location.latLng;
+      //calculate heading for the panorama image option
+      var heading = google.maps.geometry.spherical.computeHeading(
+        nearLoc, self.marker.position);
+      var lat = nearLoc.lat();
+      var lng = nearLoc.lng();
+      //StreetViewPanoramaOptions
+      var panoOptions = {
+        position: nearLoc,
+        pov: {
+          heading: heading,
+          pitch: 10
+        }
+      };
+      //getting static street image for mobile views
+      var staticImage =
+        'https://maps.googleapis.com/maps/api/streetview?size=300x300&location=' +
+        lat + ',' + lng + '&heading' + heading + '&pitch=20&key=' + myMapsKey;
+      self.imgStr = '</div><div id="infoPanoImage">No Street View Found' +
+        '</div><img id="locImg" src="' + staticImage + '">' +
+        '</div>';
+      //set content in InfoWindow
+      windowInfo.setContent(self.contentStr + self.imgStr);
+      //create Panorama image for width higher than 768px
+      var panorama = new google.maps.StreetViewPanorama(document.getElementById(
+        'infoPanoImage'), panoOptions);
+    } else {
+      self.imgStr = '</div><div id="locImg" style="min-height: auto">No Street View Found' +
+        '</div></div>';
+      windowInfo.setContent(self.contentStr + self.imgStr);
+    }
+  }; //end of getStreetView()
+
+
+  //function to assign values to the InfoWindow properties
+  this.populateInfo = function(thisMarker, infoWindow) {
+    if (infoWindow.marker != thisMarker) {
+      //using StreetViewService instead of Panorama
+      //in-case image of given latLng is not available
+      var streetView = new google.maps.StreetViewService();
+      //setting the radius to find image in meters
+      var radius = 50;
+      //default method in prototype of google.maps.StreetViewService
+      //getPanoramaByLocation(latlng, radius, callback fn)
+      streetView.getPanoramaByLocation(thisMarker.position, radius,
+        self.getStreetView);
+      //open content in infoWindow at anchor - thisMarker on 'map'
+      infoWindow.open(map, thisMarker);
+      getWiki(thisMarker);
+    }
+  }; //end of populateInfo()
+
+  //function defination to bounce pin when clicked
+  this.toggleBounce = function(pin) {
+    if (pin.getAnimation() !== null) {
+      pin.setAnimation(null);
+    } else {
+      pin.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function() {
+        pin.setAnimation(null);
+      }, 1450);
+    }
+  };
+
+
+  /*------Set of functions for Mouse events for marker pin------*/
+
+  //adding a function to hover-in event to change marker icon
+  this.marker.addListener('mouseover', function() {
+    this.setIcon(self.highlightIcon);
+  });
+  //adding a function to hover-out event to change marker icon
+  this.marker.addListener('mouseout', function() {
+    this.setIcon(self.defaultIcon);
+  });
+  //adding a function to click event to display InfoWindow for each marker
+  this.marker.addListener('click', function() {
+    self.clickMarker(this, windowInfo);
+  });
+  /*------Set of functions END------*/
+
+
+  //function defination click callback
+  //keeping this separate so that it can be called when list item is clicked
+  this.clickMarker = function(val, infoWindow) {
+    // val is the clicked marker
+    map.panTo(val.position);
+    self.toggleBounce(val);
+    self.populateInfo(val, infoWindow);
+  }; // end of function clickMarker()
+
+}; // end of locModel function DEFINATION
+
+
+
+/*============View Model===========*/
+
+var ViewModel = function() {
+  var self = this;
+
+  //function call to initialize map
+  getMap();
+
+  //initializing loactions array, Wiki data array and error message
   this.locations = ko.observableArray([]);
+  this.wikidata = ko.observableArray([]);
+  this.wikiError = ko.observable(false);
 
   //input value for searched location
   this.typedAddress = ko.observable('');
@@ -260,17 +288,11 @@ var ViewModel = function() {
 
   //function to open location infowindow on map
   self.openLocInfo = function(locInfo) {
-    //locInfo - complete LocModel with all functions
-    // clickMarker is a function that append and open content to infowindow
     locInfo.clickMarker(locInfo.marker, windowInfo);
   };
 
   //defining windowInfo before adding event listener function
-  checkinfowindow();
-  windowInfo.addListener('closeclick', function() {
-    this.close();
-    checkinfowindow();
-  });
+  initInfowindow();
 
   //function call to show all markers on map
   this.showLocations();
@@ -288,13 +310,14 @@ var ViewModel = function() {
       //return true if conditions in function are fulfilled
       //marker is also updated accordingly
       return ko.utils.arrayFilter(self.locations(), function(item) {
-        if (item.loc().toLowerCase().indexOf(filterItem) !== -1) {
+        if (item.loc.toLowerCase().indexOf(filterItem) !== -1) {
           item.marker.setVisible(true);
         } else {
           windowInfo.close();
           item.marker.setVisible(false);
+          self.wikidata([]);
         }
-        return item.loc().toLowerCase().indexOf(filterItem) !== -1;
+        return item.loc.toLowerCase().indexOf(filterItem) !== -1;
       });
     }
   }, this); // end of currentLocs computed function
@@ -312,39 +335,14 @@ var ViewModel = function() {
     self.openLocInfo(loc);
   };
 
-  this.wikidata = ko.observableArray([]);
-
-  //function to get Wikipedia data
-  this.getwiki = function() {
-    var term = 'Supernatual U.S TV Series';
-    var Wikiurl = "https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + term;
-
-    var wikiRequestTimeout = setTimeout(function() {
-      $('.wikierror').text('Failed to load Wikipedia Resources').css('display', 'block');
-    }, 1000);
-
-    $.ajax({
-      url: Wikiurl,
-      dataType: 'jsonp',
-    }).done(function(data) {
-      console.log('success');
-      self.wikidata.push({
-        title: ko.observable(data[0]),
-        src: ko.observable(data[3]),
-        snippet: ko.observable(data[2])
-      });
-      clearTimeout(wikiRequestTimeout);
-    });
-  };
-  this.getwiki();
-
   // click function for mobile search nav
   this.openList = function() {
+    //confirmed with Udacity mentor, this jQuery usage is allowed
     $('#sec_menu').toggleClass("change");
     $('#sec_list--mob').slideToggle();
   };
+}; //end of ViewModel()
 
-};
 
 
 /*============Init Function=============*/
@@ -354,5 +352,4 @@ function initMap() {
   vm = new ViewModel();
   /*=============applyig ko bindings=================*/
   ko.applyBindings(vm);
-
 }
