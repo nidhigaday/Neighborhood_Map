f67e6180024cb5c4e2922459ce6e1e2e6a60b0f6
Spacevash <nidhi.gaday@gmail.com>
2017-09-18 16:28:42 -0400

    testing

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
 /*========Google API=========*/
 var myMapsKey = 'AIzaSyAqviK9aa_gQHWt9XK8aa_EZlIQ28ScwOQ';
 
-var apiUrl = 'https://maps.googleapis.com/maps/api/js?key=' + myMapsKey + '&v=3&callback=initMap';
+var apiUrl = 'https://maps.googeapis.com/maps/api/js?key=' + myMapsKey + '&v=3&callback=initMap';
