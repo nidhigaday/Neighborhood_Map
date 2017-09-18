1597fd351c2a02cf7b65fcc60da9d340e6bbcb3c
Spacevash <nidhi.gaday@gmail.com>
2017-09-18 16:13:43 -0400

    updated locations

--- src/js/data.js
+++ src/js/data.js
@@ -3,48 +3,11 @@
 
 var data = [{
     location: {
-      lat: 49.270862,
-      lng: -123.256520
-    },
-    title: 'Phantom Traveler',
-    season: 'Season 01',
-    episode: 'Season 01: Episode 04',
-    loc: 'Green College',
-    pin: '6201 Cecil Green Park Rd, Vancouver, BC V6T 1X8',
-    city: 'Vancouver, BC'
-
-  },
-  {
-    location: {
-      lat: 49.243876,
-      lng: -122.806809
-    },
-    title: 'Bloody Mary',
-    season: 'Season 01',
-    episode: 'Season 01: Episode 05',
-    loc: 'Crease Clinic',
-    pin: '65 Pine Terrace, Coquitlam, BC V3C 5X9',
-    city: 'Coquitlam, BC'
-  },
-  {
-    location: {
-      lat: 49.2532724,
-      lng: -123.1500003
-    },
-    title: 'Hook Man',
-    season: 'Season 01',
-    episode: 'Season 01: Episode 07',
-    loc: 'Theta Sorority',
-    pin: '1974 Cedar Crescent, Vancouver, BC V6J 2R5, Canada',
-    city: 'Vancouver, BC'
-  },
-  {
-    location: {
       lat: 49.192632,
       lng: -122.969338
     },
     title: 'Bugs',
-    season: 'Season 01',
+    season: 'Season 1',
     episode: 'Season 01: Episode 08',
     loc: 'Canadian Motion Picture Park',
     pin: '8085 Glenwood Dr, Burnaby, BC V3N 5C8',
@@ -52,15 +15,15 @@ var data = [{
   },
   {
     location: {
-      lat: 49.265702,
-      lng: -123.101230
+      lat: 49.2596037,
+      lng: -122.7990194
     },
-    title: 'Bad Day At Black Rock',
-    season: 'Season 03',
-    episode: 'Season 03: Episode 03',
-    loc: '2111 Main St',
-    pin: '2111 Main St, Vancouver, BC V5T 3C6',
-    city: 'Vancouver, BC'
+    title: 'In My Time Of Dying',
+    season: 'Season 2',
+    episode: 'Season 02: Episode 01',
+    loc: 'Riverview Mental Hospital',
+    pin: '2601 Lougheed Hwy, Coquitlam, BC V4C 4J2, Canada',
+    city: 'Coquitlam, BC'
   },
   {
     location: {
@@ -68,7 +31,7 @@ var data = [{
       lng: -123.182593
     },
     title: 'Mystery Spot',
-    season: 'Season 03',
+    season: 'Season 3',
     episode: 'Season 03: Episode 11',
     loc: 'George\'s Taverna',
     pin: '12160-12242 First Ave, Richmond, BC V7E, Canada',
@@ -80,7 +43,7 @@ var data = [{
       lng: -123.092142
     },
     title: 'Monster Movie',
-    season: 'Season 04',
+    season: 'Season 4',
     episode: 'Season 04: Episode 05',
     loc: 'Former Fantasy Gardens',
     pin: '11760 Steveston Hwy, Richmond, BC V7A, Canada',
@@ -88,27 +51,15 @@ var data = [{
   },
   {
     location: {
-      lat: 49.228081,
-      lng: -122.995914
+      lat: 49.239625,
+      lng: -122.9656191
     },
-    title: 'The Curious Case Of Dean Winchester',
-    season: 'Season 05',
-    episode: 'Season 05: Episode 07',
-    loc: '4808 Hazel St.',
-    pin: '4808 Hazel St. Burnaby, BC V5H 4T3',
+    title: 'Changing Channels',
+    season: 'Season 5',
+    episode: 'Season 05: Episode 08',
+    loc: 'Burnaby Village Museum',
+    pin: '6501 Deer Lake Ave, Burnaby, BC V5G 3T6, Canada',
     city: 'Burnaby, BC'
-  },
-  {
-    location: {
-      lat: 49.203568,
-      lng: -122.912689
-    },
-    title: 'Hammer of the Gods',
-    season: 'Season 05',
-    episode: 'Season 05: Episode 19',
-    loc: 'The Metro Hall',
-    pin: '700 Royal Ave, New Westminster, BC V3M 5Z5, Canada',
-    city: 'New Westminster, BC'
   },
   {
     location: {
@@ -116,7 +67,7 @@ var data = [{
       lng: -123.150903
     },
     title: 'The Third Man',
-    season: 'Season 06',
+    season: 'Season 6',
     episode: 'Season 06: Episode 03',
     loc: 'Casa Mia',
     pin: '1920 SW Marine Dr, Vancouver, BC V6P 6B2',
@@ -124,15 +75,15 @@ var data = [{
   },
   {
     location: {
-      lat: 49.104432,
-      lng: -122.7342177
+      lat: 49.103639,
+      lng: -122.652862
     },
-    title: 'We Need to Talk About Kevin',
-    season: 'Season 08',
-    episode: 'Season 08: Episode 01',
-    loc: 'Cloverdale Service',
-    pin: '17625 56 Ave, Surrey, BC V3S 1C6, Canada',
-    city: 'Surrey, BC'
+    title: 'Slash Fiction',
+    season: 'Season 7',
+    episode: 'Season 07: Episode 06',
+    loc: 'Credit Unions of BC​',
+    pin: '20550 Fraser Hwy, Langley Twp, BC V3A 4G2, Canada',
+    city: 'Langley, BC'
   },
   {
     location: {
@@ -140,10 +91,58 @@ var data = [{
       lng: -122.845442
     },
     title: 'Sacrifice',
-    season: 'Season 08',
+    season: 'Season 8',
     episode: 'Season 08: Episode 23',
     loc: 'Compass Point Inn',
     pin: '9850 King George Hwy, Surrey, BC V3T',
+    city: 'Surrey, BC'
+  },
+  {
+    location: {
+      lat: 49.149775,
+      lng: -122.845167
+    },
+    title: 'Rock And A Hard Place',
+    season: 'Season 9',
+    episode: 'Season 09: Episode 08',
+    loc: 'Kalmar Restaurant ',
+    pin: '8076 King George Blvd, Surrey, BC V3W 5B5, Canada',
+    city: 'Surrey, BC'
+  },
+  {
+    location: {
+      lat: 49.089348,
+      lng: -123.041458
+    },
+    title: 'Black',
+    season: 'Season 10',
+    episode: 'Season 10: Episode 01',
+    loc: 'Sundance Pub',
+    pin: '6574 Ladner Trunk Rd, Delta, BC V4K 3N3, Canada',
+    city: 'Delta, BC'
+  },
+  {
+    location: {
+      lat: 49.2762265,
+      lng: -123.1236937
+    },
+    title: 'Just My Imagination',
+    season: 'Season 11',
+    episode: 'Season 11: Episode 08',
+    loc: 'Emery Barnes Park',
+    pin: '1170 Richards St, Vancouver, BC V6B 5B6, Canada',
+    city: 'Vancouver, BC'
+  },
+  {
+    location: {
+      lat: 49.0641553,
+      lng: -122.8164795
+    },
+    title: 'Lotus',
+    season: 'Season 12',
+    episode: 'Season 12: Episode 08',
+    loc: 'Timberland Hotel & Campground',
+    pin: '3418 King George Blvd, Surrey, BC V4P 1A8, Canada',
     city: 'Surrey, BC'
   }
 ];
